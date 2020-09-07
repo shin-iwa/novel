3aae133a50a52137e0aafc09b48e5b7fd36c0bba
shin-iwa <luckystar0443@gmail.com>
2020-09-07 11:27:56 +0900

    updata jquery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :likes
   has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
